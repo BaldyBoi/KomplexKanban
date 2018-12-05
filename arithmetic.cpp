9e3ae593fc3aff07ffaee0f62b08861c6f34a5be
BaldyBoi <45365854+BaldyBoi@users.noreply.github.com>
2018-12-05 11:48:03 -0600

    Update arithmetic.cpp
    Not done just saving draft

--- arithmetic.cpp
+++ arithmetic.cpp
@@ -1,32 +1,58 @@
+/******************************************************** 
+----Creater: Alex Yang
+----Modified: Ethan Baldonado. December 5th, 2018
+----Overview: Creates a CPP file containing function to
+elegantly handle imaginary numbers using different 
+operators.
+********************************************************/
 #include <math.h>
 #include "complex.h"
 
 #define PI 3.1415926538979323846
 
+/*
+returns absolute value of imaginary number
+*/
+
 double abs(const Complex me) {
+    //the real part of the number (e.g 3 + 6i, returns 3)
     double re = real(me);
+    //the imaginary part of the number
     double im = imag(me);
     return sqrt(re*re + im*im);
 }
-
+/*
+returns the arctangent of complex number com in radians
+*/
 double arg(const Complex com) {
     double ret = atan(imag(com)/real(com));
+    //Only case where the division is not in the domain. Division of objects in 2nd and 4th quadrant is the same. 
     if(imag(com) < 0 && real(com) < 0) {
-        //arcTangent has a limited domain. Check both components of com and change angle accordingly
+        //arcTangent has a limited domain. adding pi will guarnteee that it is within the domain.
         ret -= PI;
     }
     return ret;
 }
 
+/*
+returns conjugate
+*/
 Complex conj(const Complex com) {
+    //only needs to change the imaginary part of com
     Complex ret(real(com), -1*imag(com));
     return ret;
 }
 
+/*
+returns conjugate of &the
+*/
 double imag(const Complex &the) {
     return the.im;
 }
 
+/*
+makes number non imaginary
+*/
 double norm(const Complex com) {
     double re(real(com)), im(imag(com));//Fancy initialization
     return re*re+im*im;
